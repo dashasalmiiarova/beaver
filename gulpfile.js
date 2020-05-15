8365d717894e47eeac61d5c9d63555d3804a31a2
Dariia <dashasalmiiarova@icloud.com>
2020-05-15 02:25:43 +0200

    first

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+let
+    gulp = require("gulp"),
+    browserSync = require('browser-sync'),
+    gulpCssmin = require('gulp-cssmin'),
+    gulpRename = require('gulp-rename');
+
+    gulp.task('mincss', function(){
+        return gulp.src('app/css/style.css')
+            .pipe(gulpCssmin())
+            .pipe(gulpRename({suffix: '.min'}))
+            .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.reload({stream: true}))
+    });
+    gulp.task('js', function(){
+        return gulp.src([
+            'node_modules/countup.js/dist/countUp.min.js'
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({stream: true}))
+    });
+    gulp.task('html', function(){
+        return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream: true}))
+    });
+    gulp.task('js', function(){
+        return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({stream: true}))
+    });
+    gulp.task('browser-sync', function() {
+        browserSync.init({
+            server: {
+                baseDir: "app/"
+            }
+        });
+    });
+    gulp.task('watch', function(){
+        gulp.watch('app/css/style.css', gulp.parallel('mincss'))
+        gulp.watch('app/*.html', gulp.parallel('html'))
+        gulp.watch('app/js/*.js', gulp.parallel('js'))
+    });
+    gulp.task('default', gulp.parallel('mincss', 'js', 'browser-sync', 'watch'))
